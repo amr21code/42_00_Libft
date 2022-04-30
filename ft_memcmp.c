c1559a3bd82a0c27d8760d04d4cc00f9977e6a9e
amr21code <andi@noshut.de>
2022-04-30 13:43:34 +0200

    ft_memcmp changed to mac behavior

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -25,14 +25,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int				cmp;
 	size_t			i;
 	unsigned char	*str1;
 	unsigned char	*str2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	cmp = 0;
 	i = 0;
 	while (i < n)
 	{
@@ -40,15 +38,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			return (str1[i] - str2[i]);
 		i++;
 	}
-	return (cmp);
-}
-#include <stdio.h>
-
-int	main(void)
-{
-	char str1[] = "";
-	char str2[] = "M";
-	
-	printf("%d\n", ft_memcmp((const void *)str1, (const void *)str2, -1));
 	return (0);
 }
